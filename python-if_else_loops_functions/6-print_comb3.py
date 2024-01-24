7319a0c2a33cb90a8fc13e22a49d92ef2bd51e1d
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-01-24 09:07:13 +0200

    Update 6-print_comb3.py

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+for tens_digit in range(10):
+    for units_digit in range(tens_digit + 1, 10):
+        print("{:d}{:d}".format(tens_digit, units_digit), end=', ' if units_digit < 9 else '\n')
