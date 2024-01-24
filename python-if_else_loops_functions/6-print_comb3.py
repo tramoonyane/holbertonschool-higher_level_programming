01053a8dd169dc4a0e081c2f11609b8697d85047
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-01-24 12:22:03 +0200

    Update 6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-
-for tens_digit in range(10):
-    for units_digit in range(tens_digit + 1, 10):
-        print("{:d}{:d}".format(tens_digit, units_digit), end=', ' if units_digit < 9 else '\n')
+for x in range(0, 10):
+    for y in range(x + 1, 10):
+        if x == 8 and y == 9:
+            print('89')
+        else:
+            print('{}{}, '.format(x, y), end='')
