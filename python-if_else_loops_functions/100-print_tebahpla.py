ffc541873725bc9f2042c56174b3223fb4859c4c
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-01-24 13:05:04 +0200

    Update 100-print_tebahpla.py

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 for i in range(ord('z'), ord('a') - 1, -1):
     if i % 2 == 0:
-        diff = 0
-    else:
         diff = 32
+    else:
+        diff = 0
     print('{}'.format(chr(i - diff)), end='')
+
+# Add a newline at the end for better readability
+print()
