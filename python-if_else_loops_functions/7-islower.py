a76ec0714b7b8dbc0d100ae0f92c4af025b33014
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-01-24 12:27:15 +0200

    Update 7-islower.py

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    # Check if the ASCII value of the character is between the ASCII values of 'a' and 'z'
-    return ord('a') <= ord(c) <= ord('z')
-
-# Test the function
-print(islower('a'))  # True
-print(islower('Z'))  # False
-print(islower('1'))  # False
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
