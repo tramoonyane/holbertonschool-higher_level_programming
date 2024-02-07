119d5058109b6035bfa621a47499db5070552b16
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-02-08 00:01:57 +0200

    Update 4-inherits_from.py

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
-'''Module for inherits_from method.'''
-
 def inherits_from(obj, a_class):
-    '''Determines if an object is a true subclass of a class.'''
-    return issubclass(type(obj), a_class) and type(obj) is not a_class
+    """
+    Check if the object is an instance of a class that inherited (directly or indirectly) from the specified class.
+
+    :param obj: The object to check.
+    :param a_class: The class to check against.
+    :return: True if obj is an instance of a class that inherited from a_class; otherwise, False.
+    """
+    # Get the class of the object
+    obj_class = type(obj)
+
+    # Check if the object's class is a subclass of the specified class
+    return issubclass(obj_class, a_class)
