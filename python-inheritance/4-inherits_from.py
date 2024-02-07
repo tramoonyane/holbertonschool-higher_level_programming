21e6382f3c79de768449bcabf5bcdd02e446b4b2
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-02-08 00:56:58 +0200

    Update 4-inherits_from.py

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -12,4 +12,4 @@ def inherits_from(obj, a_class):
         If obj is exactly an instance of a_class - True.
         Otherwise - False.
     """
-    return isinstance(obj, type) and issubclass(obj, a_class)
+    return type(obj) is not a_class and issubclass(type(obj), a_class)
