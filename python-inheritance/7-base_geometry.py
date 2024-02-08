95d59b1862f3fc01eb8677ee98af7d2db48f27f1
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-02-08 03:33:38 +0200

    Update 7-base_geometry.py

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -13,11 +13,10 @@ class BaseGeometry:
             name (str): The name of the parameter.
             value (int): The parameter to validate.
         Raises:
-            TypeError: If value is not an integer or is None.
+            TypeError: If value is not an integer.
             ValueError: If value is <= 0.
         """
-        if not isinstance(value, int) or value is None:
+        if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
-
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
