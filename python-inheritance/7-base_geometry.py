7d5812b2a1092d7748419500e4ed33c7bde1f418
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-02-08 12:59:04 +0200

    Update 7-base_geometry.py

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 
 class BaseGeometry:
+    def area(self):
+        raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
         if not isinstance(value, (int, bool)):
-            raise TypeError(f"{name} must be an integer or boolean")
+            raise TypeError(f"{name} must be an integer")
 
         if isinstance(value, int) and value <= 0:
-            raise ValueError(f"{name} must be an integer greater than 0")
-
-        if isinstance(value, bool):
-            raise TypeError(f"{name} must be a boolean, not an integer")
-
-        if value is not None and not isinstance(value, (int, bool)):
-            raise TypeError(f"{name} must be an integer or boolean, not {type(value).__name__}")
+            raise ValueError(f"{name} must be greater than 0")
