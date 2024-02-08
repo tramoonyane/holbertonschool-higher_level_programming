9bcb88f8778e40cc59428a99e8351adfaa3bb47f
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-02-08 03:30:46 +0200

    Update 7-base_geometry.py

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -16,10 +16,8 @@ class BaseGeometry:
             TypeError: If value is not an integer or is None.
             ValueError: If value is <= 0.
         """
-        if not name or value is None:
-            raise TypeError(name + " must be an integer")
+        if not isinstance(value, int) or value is None:
+            raise TypeError("{} must be an integer".format(name))
 
-        if type(value) != int:
-            raise TypeError(name + " must be an integer")
         if value <= 0:
-            raise ValueError(name + " must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
