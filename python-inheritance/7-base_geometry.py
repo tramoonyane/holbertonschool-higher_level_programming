0e6f441a2402be56e9c00c897c4380dc83a51ad0
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-02-08 09:32:08 +0200

    Update 7-base_geometry.py

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -14,11 +14,4 @@ class BaseGeometry:
         """Placeholder for calculating the area."""
         raise Exception("area() is not implemented")
 
-    def integer_validator(self, name, value):
-        """Validate the 'value' parameter as an integer."""
-        if not isinstance(name, str):
-            raise TypeError("name must be a string")
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+   
