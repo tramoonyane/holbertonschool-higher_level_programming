449288948707eef8e5987de508f1602f13ff318e
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-02-08 09:40:45 +0200

    Update 7-base_geometry.py

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 class BaseGeometry:
-
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
 
 
    
