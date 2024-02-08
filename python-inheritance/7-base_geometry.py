30e3d964a0c6db4ccbf826f07812726fdd8a7298
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-02-08 04:16:04 +0200

    Update 7-base_geometry.py

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,28 +1,38 @@
 #!/usr/bin/python3
+"""
+7-base_geometry.py
+
+Defines a class BaseGeometry with two public instance methods: area() and integer_validator().
+"""
 
 class BaseGeometry:
     """
-    BaseGeometry class with area and integer_validator methods.
+    A class representing Base Geometry.
+
+    Methods:
+    - area(self): Raises an Exception with the message 'area() is not implemented'.
+    - integer_validator(self, name, value): Validates the given integer value.
     """
     def area(self):
         """
-        Raises an Exception with the message 'area() is not implemented'.
+        Raises:
+        - Exception: 'area() is not implemented'.
         """
-        raise Exception("area() is not implemented")
+        raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
         """
-        Validates that 'value' is a positive integer.
+        Validates the given integer value.
 
         Args:
-            name (str): Name of the value being validated.
-            value (int): Value to be validated.
+        - name (str): The name of the value.
+        - value (int): The value to be validated.
 
         Raises:
-            TypeError: If 'value' is not an integer.
-            ValueError: If 'value' is not greater than 0.
+        - TypeError: If the value is not an integer. The message will be '<name> must be an integer'.
+        - ValueError: If the value is less than or equal to 0. The message will be '<name> must be greater than 0'.
         """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
+        if not isinstance(value, int):
+            raise TypeError('{} must be an integer'.format(name))
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError('{} must be greater than 0'.format(name))
