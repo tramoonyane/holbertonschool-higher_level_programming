4a87b0840c4d7a8f133167b533f37c3ff0d1b7fb
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-02-09 12:27:30 +0200

    Update 8-class_to_json.py

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python3
-"""Defines a Python class-to-JSON function."""
-
 
 def class_to_json(obj):
-    """Return the dictionary represntation of a simple data structure."""
-    return obj.__dict__
+    """
+    Converts an instance of a class to a dictionary description
+    with simple data structures for JSON serialization.
+
+    Parameters:
+    obj (object): An instance of a Class with serializable attributes.
+
+    Returns:
+    dict: A dictionary containing the serializable attributes of the object.
+    """
+    if not hasattr(obj, '__dict__'):
+        raise ValueError("Input object is not an instance of a class")
+
+    serializable_data = {}
+    for key, value in obj.__dict__.items():
+        if isinstance(value, (list, dict, str, int, bool)):
+            serializable_data[key] = value
+
+    return serializable_data
