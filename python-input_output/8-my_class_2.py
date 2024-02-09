3695cda3f0daa18482c6f990a4e08cc9aa025652
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-02-09 12:38:57 +0200

    Update 8-my_class_2.py

--- python-input_output/8-my_class_2.py
+++ python-input_output/8-my_class_2.py
@@ -6,9 +6,18 @@ class MyClass:
     """ My class
     """
 
-    def __init__(self, name):
-        self.name = name
-        self.number = 0
+    score = 0
+
+    def __init__(self, name, number = 4):
+        self.__name = name
+        self.number = number
+        self.is_team_red = (self.number % 2) == 0
+
+    def win(self):
+        self.score += 1
+
+    def lose(self):
+        self.score -= 1
 
     def __str__(self):
-        return "[MyClass] {} - {:d}".format(self.name, self.number)
+        return "[MyClass] {} - {:d} => {:d}".format(self.__name, self.number, self.score)
