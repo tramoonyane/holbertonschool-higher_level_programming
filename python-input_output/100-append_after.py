63233928929ea4bdfe85b9ee4a067f2cdcd98b6e
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-02-10 15:15:08 +0200

    Update 100-append_after.py

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -6,7 +6,7 @@ after each line containing a specific string.
 """
 
 def append_after(filename="", search_string="", new_string=""):
-     """
+    """
     Inserts a line of text to a file after each line containing a specific string.
 
     :param filename: The name of the file to be modified.
