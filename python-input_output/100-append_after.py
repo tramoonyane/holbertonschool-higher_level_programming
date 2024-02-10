7d595b1d38bdbb6dc7010258da9cfebaa8c1097d
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-02-10 16:02:56 +0200

    Update 100-append_after.py

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -1,45 +1,23 @@
 #!/usr/bin/python3
+""" Module that executes a function that appends a line """
 
-"""
-This module defines a function to insert a line of text into a file after each line
-containing a specific string.
-
-Functions:
-    - append_after(filename, search_string, new_string):
-        Inserts a line of text to a file after each line containing a specific string.
-
-        :param filename: The name of the file to be modified.
-        :param search_string: The string to search for in each line.
-        :param new_string: The string to be inserted after each line containing the search string.
-        :return: None
-"""
 
 def append_after(filename="", search_string="", new_string=""):
+    """ Function that appends a new line when a string is found
+
+    Args:
+        filename: filename
+        search_string: string to search
+        new_string: string to append
+
     """
-    Inserts a line of text to a file after each line containing a specific string.
 
-    :param filename: The name of the file to be modified.
-    :param search_string: The string to search for in each line.
-    :param new_string: The string to be inserted after each line containing the search string.
-    """
-    # Check if any of the parameters is missing
-    if not filename or not search_string or not new_string:
-        print("Please provide valid inputs for filename, search_string, and new_string.")
-        return
+    res_line = []
+    with open(filename, 'r', encoding="utf-8") as f:
+        for line in f:
+            res_line += [line]
+            if line.find(search_string) != -1:
+                res_line += [new_string]
 
-    # Open the file in read mode
-    with open(filename, 'r') as file:
-        # Read all lines from the file
-        lines = file.readlines()
-
-    # Open the file in write mode
-    with open(filename, 'w') as file:
-        # Iterate through the lines
-        for line in lines:
-            # Write the current line to the file
-            file.write(line)
-            
-            # Check if the search string is present in the current line
-            if search_string in line:
-                # If found, append the new string after the line
-                file.write(new_string + '\n')
+    with open(filename, 'w', encoding="utf-8") as f:
+        f.write("".join(res_line))
