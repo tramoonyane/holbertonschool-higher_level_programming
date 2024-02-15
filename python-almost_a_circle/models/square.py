d2f85dcff96937e7cab9b00c35e58653ff6748a6
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-02-15 14:42:46 +0200

    Update square.py

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -8,12 +8,18 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     """
-    ...
+    Represent a square
     """
 
     def __init__(self, size, x=0, y=0, id=None):
         """
-        ...
+        Initialize a new Square.
+
+        Args:
+            size (int): The size of the new Square.
+            x (int): The x coordinate of the new Square.
+            y (int): The y coordinate of the new Square.
+            id (int): The identity of the new Square.
         """
         super().__init__(size, size, x, y, id)
 
@@ -28,21 +34,26 @@ class Square(Rectangle):
     @property
     def size(self):
         """
-        ...
+        Get/set the size of the Square.
         """
         return self.width
 
     @size.setter
     def size(self, value):
-        """
-        ...
-        """
         self.width = value
         self.height = value
 
     def update(self, *args, **kwargs):
         """
-        ...
+        Update the Square.
+
+        Args:
+            *args (ints): New attribute values.
+                - 1st argument represents id attribute
+                - 2nd argument represents size attribute
+                - 3rd argument represents x attribute
+                - 4th argument represents y attribute
+            **kwargs (dict): New key/value pairs of attributes.
         """
         argc = len(args)
         kwargc = len(kwargs)
@@ -61,7 +72,7 @@ class Square(Rectangle):
 
     def to_dictionary(self):
         """
-        ...
+        Return the dictionary representation of the Square.
         """
         return {
             'id': self.id,
