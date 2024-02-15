839acc0208f1d9a9fba36336837491781871b646
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-02-15 22:55:48 +0200

    Update square.py

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
 """
-Defines a square class.
+This module contains the "Square" class
 """
-
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """
-    Represent a square
-    """
+    """Represent a square."""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """
-        Initialize a new Square.
-
+        """Initialize a new Square.
         Args:
             size (int): The size of the new Square.
             x (int): The x coordinate of the new Square.
@@ -23,25 +18,9 @@ class Square(Rectangle):
         """
         super().__init__(size, size, x, y, id)
 
-    def __str__(self):
-        """
-        Returns a string representation of the Square instance.
-
-        The string includes information about the Square's identity (id),
-        coordinates (x, y), and size (width).
-
-        Returns:
-        str: A string in the format '[Square] (id) x/y - size'.
-        """
-        return '[Square] ({:d}) {:d}/{:d} - {:d}'.format(
-            self.id, self.x, self.y, self.width
-    )
-
     @property
     def size(self):
-        """
-        Get/set the size of the Square.
-        """
+        """Get/set the size of the Square."""
         return self.width
 
     @size.setter
@@ -50,9 +29,7 @@ class Square(Rectangle):
         self.height = value
 
     def update(self, *args, **kwargs):
-        """
-        Update the Square.
-
+        """Update the Square.
         Args:
             *args (ints): New attribute values.
                 - 1st argument represents id attribute
@@ -61,28 +38,46 @@ class Square(Rectangle):
                 - 4th argument represents y attribute
             **kwargs (dict): New key/value pairs of attributes.
         """
-        argc = len(args)
-        kwargc = len(kwargs)
-        modif_attrs = ['id', 'size', 'x', 'y']
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.size = arg
+                elif a == 2:
+                    self.x = arg
+                elif a == 3:
+                    self.y = arg
+                a += 1
 
-        if argc > 4:
-            argc = 4
-
-        if argc > 0:
-            for i in range(argc):
-                setattr(self, modif_attrs[i], args[i])
-        elif kwargc > 0:
+        elif kwargs and len(kwargs) != 0:
             for k, v in kwargs.items():
-                if k in modif_attrs:
-                    setattr(self, k, v)
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "size":
+                    self.size = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
 
     def to_dictionary(self):
-        """
-        Return the dictionary representation of the Square.
-        """
+        """Return the dictionary representation of the Square."""
         return {
-            'id': self.id,
-            'size': self.size,
-            'x': self.x,
-            'y': self.y
+            "id": self.id,
+            "size": self.width,
+            "x": self.x,
+            "y": self.y
         }
+
+    def __str__(self):
+        """Return the print() and str() representation of a Square."""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                 self.width)
