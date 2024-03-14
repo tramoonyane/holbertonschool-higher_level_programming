c4ee95e29a4c7bc5afcd1ab905cbe61f93a7f8c8
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-03-14 15:35:33 +0200

    Update 5-full_table.sql

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,6 +1,17 @@
--- Script to print the description of the table first_table from the database hbtn_0c_0 in MySQL server.
+#!/bin/bash
 
--- Query to fetch the description of the table first_table
-SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, IS_NULLABLE
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+# Script to print the description of the table first_table from the database hbtn_0c_0 in MySQL server
+
+# Check if the required argument is provided
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <database_name>"
+    exit 1
+fi
+
+# Assign the argument to a variable
+database=$1
+
+# MySQL query to retrieve table structure
+echo "SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY, EXTRA
+           FROM INFORMATION_SCHEMA.COLUMNS
+           WHERE TABLE_SCHEMA = '$database' AND TABLE_NAME = 'first_table';"
