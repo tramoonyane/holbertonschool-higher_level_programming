ca0df56aaf26cdd22c823d3317bc8b81d0842a19
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-03-14 14:49:19 +0200

    Update 5-full_table.sql

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,6 +1,4 @@
--- SQL script to print the description of the table first_table in a specific database
+-- SQL script to print the full description of the table first_table in a specific database
 
--- Query to retrieve table schema information
-SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+-- Query to retrieve the full table creation statement
+SHOW CREATE TABLE hbtn_0c_0.first_table;
