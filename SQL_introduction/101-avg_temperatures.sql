4079a856a661f1b600821907b0b78e8b6656bdfa
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-03-15 00:32:40 +0200

    Update 101-avg_temperatures.sql

--- SQL_introduction/101-avg_temperatures.sql
+++ SQL_introduction/101-avg_temperatures.sql
@@ -1,6 +1,5 @@
--- Query retrieves the average temperature for each city from the temperatures table.
--- The results are ordered in descending order of average temperature.
-SELECT city, AVG(value) AS average_temperature
-FROM temperatures
-GROUP BY city
-ORDER BY average_temperature DESC;
+-- Displays the average temperature (in Fahrenheit) by city ordered by descending temperature.
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+GROUP BY `city`
+ORDER BY `avg_temp` DESC;
