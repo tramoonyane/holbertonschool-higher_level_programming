17b69d373a7ad9e0bf88f7eadbbb5ac03b1e6ce0
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-03-14 15:06:16 +0200

    Update 5-full_table.sql

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,7 @@
--- SQL script to print the full description of the table first_table in a specific database
+--Script to print the description of the table first_table from the database hbtn_0c_0 in MySQL server.
 
--- Query to retrieve the full table creation statement
-SHOW CREATE TABLE hbtn_0c_0.first_table;
+-- Query to fetch the description of the table first_table
+
+SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, IS_NULLABLE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
