bcbb0c412a78ddf3b34240b4c3e16baa9f142a03
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-03-15 16:48:06 +0200

    Update 10-genre_id_by_show.sql

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+-- SQL query to list all shows with at least one genre linked in the hbtn_0d_tvshows database
+
+-- Query to list all shows with at least one genre linked and sort by title and genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
