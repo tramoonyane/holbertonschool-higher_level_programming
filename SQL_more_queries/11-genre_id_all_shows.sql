b9e94bba31c4b61c9f9e00a3d2f57b4de0b0d7c2
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-03-16 01:48:19 +0200

    Update 11-genre_id_all_shows.sql

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -3,5 +3,5 @@
 -- Query to list all shows with their corresponding genre_id (or NULL if no genre linked) and sort by title and genre_id
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
