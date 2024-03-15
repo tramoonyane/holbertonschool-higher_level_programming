782adeac5702f016b0557802ffdbf2fb356dfae4
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-03-16 01:50:06 +0200

    Update 12-no_genre.sql

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -3,6 +3,6 @@
 -- Query to list all shows without a linked genre and sort by title and genre_id
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.genre_id IS NULL
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
