3d22624722974802bafd88bb50ea3f7860c4051e
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-03-15 20:22:06 +0200

    Update 1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -2,6 +2,5 @@
 
 -- Query to create user user_0d_1 with all privileges and set password
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
--- Query to grant all privileges to user_0d_1
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
