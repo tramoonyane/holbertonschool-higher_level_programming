fc8a6277b36151b0b7f0f1bee5e7f61751a5c220
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-03-16 02:50:15 +0200

    Update 102-rating_shows.sql

--- SQL_more_queries/102-rating_shows.sql
+++ SQL_more_queries/102-rating_shows.sql
@@ -1,7 +1,7 @@
 -- SQL query to list all shows from hbtn_0d_tvshows_rate by their rating
 
 -- Query to list all shows by their rating sum
-SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating_sum
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
 FROM tv_shows
 JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
 GROUP BY tv_shows.title
