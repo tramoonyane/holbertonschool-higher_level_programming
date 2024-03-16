aee98e9e16815cc9aca79c716579e9b8e9211e97
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-03-16 02:50:38 +0200

    Update 103-rating_genres.sql

--- SQL_more_queries/103-rating_genres.sql
+++ SQL_more_queries/103-rating_genres.sql
@@ -1,7 +1,7 @@
 -- SQL query to list all genres in the database hbtn_0d_tvshows_rate by their rating
 
 -- Query to list all genres by their rating sum
-SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating_sum
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
