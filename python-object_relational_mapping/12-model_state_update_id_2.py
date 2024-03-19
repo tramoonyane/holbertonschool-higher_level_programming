474ed019dbe1436ca84b329aa9426c9c2ba65d64
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-03-19 23:48:22 +0200

    Update 12-model_state_update_id_2.py

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -8,10 +8,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
+
 def update_state_name(username, password, database):
     """
-    Connects to the MySQL server and changes the name of the State object with id = 2 to "New Mexico".
-    
+    Connects to the MySQL server and changes the name
+    of the State object with id = 2 to "New Mexico".
+
     Args:
         username (str): MySQL username.
         password (str): MySQL password.
@@ -19,24 +21,32 @@ def update_state_name(username, password, database):
     """
     try:
         # Connect to the MySQL server
-        engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(username, password, database),
-                               pool_pre_ping=True)
+        engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                username, password, database
+            ),
+            pool_pre_ping=True
+        )
         Base.metadata.create_all(engine)
         Session = sessionmaker(bind=engine)
         session = Session()
-        
+
         # Query the State object with id = 2 and update its name
         state_to_update = session.query(State).filter_by(id=2).first()
         if state_to_update:
             state_to_update.name = "New Mexico"
             session.commit()
-        
+
         session.close()
     except Exception as e:
         print("Error:", e)
+
 
 if __name__ == "__main__":
     if len(sys.argv) == 4:
         update_state_name(sys.argv[1], sys.argv[2], sys.argv[3])
     else:
-        print("Usage: ./12-model_state_update_id_2.py <username> <password> <database>")
+        print("""
+        Usage: ./12-model_state_update_id_2.py <username> <password>
+        <database>
+        """)
