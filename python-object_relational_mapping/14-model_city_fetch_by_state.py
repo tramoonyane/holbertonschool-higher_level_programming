36b9457b0b0c21855b6e51bd80545fdd83283913
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-03-20 00:03:56 +0200

    Update 14-model_city_fetch_by_state.py

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -10,10 +10,12 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 from model_city import City
 
+
 def fetch_cities_by_state(username, password, database):
     """
-    Connects to the MySQL server and prints all City objects grouped by state from the specified database.
-    
+    Connects to the MySQL server and prints all City
+    objects grouped by state from the specified database.
+
     Args:
         username (str): MySQL username.
         password (str): MySQL password.
@@ -21,23 +23,31 @@ def fetch_cities_by_state(username, password, database):
     """
     try:
         # Connect to the MySQL server
-        engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(username, password, database),
-                               pool_pre_ping=True)
+        engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                username, password, database
+            ),
+            pool_pre_ping=True
+        )
         Base.metadata.create_all(engine)
         Session = sessionmaker(bind=engine)
         session = Session()
-        
+
         # Query and print all City objects grouped by state
         cities = session.query(City, State).join(State).order_by(City.id).all()
         for city, state in cities:
             print("{}: ({}) {}".format(state.name, city.id, city.name))
-        
+
         session.close()
     except Exception as e:
         print("Error:", e)
+
 
 if __name__ == "__main__":
     if len(sys.argv) == 4:
         fetch_cities_by_state(sys.argv[1], sys.argv[2], sys.argv[3])
     else:
-        print("Usage: ./14-model_city_fetch_by_state.py <username> <password> <database>")
+        print("""
+        Usage: ./14-model_city_fetch_by_state.py <username> <password>
+        <database>
+        """)
