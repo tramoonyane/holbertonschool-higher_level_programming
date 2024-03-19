af3d060f15d76b5498f278f3c3a38230181ab39d
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-03-19 18:27:14 +0200

    Update 4-cities_by_state.py

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -6,10 +6,12 @@ Script to list all cities from the database hbtn_0e_4_usa.
 import MySQLdb
 import sys
 
+
 def list_cities(username, password, database):
     """
-    Connects to the MySQL server and lists all cities from the specified database.
-    
+    Connects to the MySQL server and lists
+    all cities from the specified database.
+
     Args:
         username (str): MySQL username.
         password (str): MySQL password.
@@ -39,8 +41,12 @@ def list_cities(username, password, database):
     except MySQLdb.Error as e:
         print("MySQL Error:", e)
 
+
 if __name__ == "__main__":
     if len(sys.argv) == 4:
         list_cities(sys.argv[1], sys.argv[2], sys.argv[3])
     else:
-        print("Usage: ./4-cities_by_state.py <username> <password> <database>")
+        print("""
+        Usage: ./4-cities_by_state.py <username>
+        <password> <database>
+        """)
