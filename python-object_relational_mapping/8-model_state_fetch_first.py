e35171b38cc35b0ef39e919b005e19178921c85b
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-03-19 23:39:19 +0200

    Update 8-model_state_fetch_first.py

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Script to print the first State object from the database hbtn_0e_6_usa.
+Script to print the first State object
+from the database hbtn_0e_6_usa.
 """
 
 import sys
@@ -8,10 +9,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
+
 def fetch_first_state(username, password, database):
     """
-    Connects to the MySQL server and prints the first State object from the specified database.
-    
+    Connects to the MySQL server and prints
+    the first State object from the specified database.
+
     Args:
         username (str): MySQL username.
         password (str): MySQL password.
@@ -19,25 +22,33 @@ def fetch_first_state(username, password, database):
     """
     try:
         # Connect to the MySQL server
-        engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(username, password, database),
-                               pool_pre_ping=True)
+        engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                username, password, database
+            ),
+            pool_pre_ping=True
+        )
         Base.metadata.create_all(engine)
         Session = sessionmaker(bind=engine)
         session = Session()
-        
+
         # Query the first State object and display it
         first_state = session.query(State).order_by(State.id).first()
         if first_state:
             print("{}: {}".format(first_state.id, first_state.name))
         else:
             print("Nothing")
-        
+
         session.close()
     except Exception as e:
         print("Error:", e)
+
 
 if __name__ == "__main__":
     if len(sys.argv) == 4:
         fetch_first_state(sys.argv[1], sys.argv[2], sys.argv[3])
     else:
-        print("Usage: ./8-model_state_fetch_first.py <username> <password> <database>")
+        print("""
+        Usage: ./8-model_state_fetch_first.py <username> <password>
+        <database>
+        """)
