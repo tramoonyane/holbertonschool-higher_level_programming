ad1b39110599c149d926674bd338c3e7c8ac2ad2
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-03-20 00:46:52 +0200

    Update 7-cities.sql

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,15 +1,7 @@
--- SQL script to create the database hbtn_0d_usa and the table cities on the MySQL server
-
--- Query to create the database if it does not already exist
+-- creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server
+-- creates a database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-
--- Query to use the database hbtn_0d_usa
+-- use a database
 USE hbtn_0d_usa;
-
--- Query to create the table if it does not already exist
-CREATE TABLE IF NOT EXISTS cities (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    state_id INT NOT NULL,
-    name VARCHAR(256) NOT NULL
-    FOREIGN KEY (state_id) REFERENCES states(id)
-);
+-- creates a table
+CREATE TABLE IF NOT EXISTS states (id INT UNIQUE NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
